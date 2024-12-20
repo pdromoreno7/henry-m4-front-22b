c29e4f13f3bd73377094e73f484e76c0f8f2a0fa
pdromoreno7 <pdromorenodev@gmail.com>
2024-12-19 19:22:38 -0500

    fix: next type params

--- src/app/product/[productID]/page.tsx
+++ src/app/product/[productID]/page.tsx
@@ -1,19 +1,22 @@
 import ProductDetail from "@/app/components/productDetail";
-import { NextPage } from "next";
 
-interface ProductPageProps {
-  params: {
-    productID: string;
-  };
-}
+// Definir el tipo específico para los parámetros de la página
+type PageParams = {
+  productID: string;
+};
 
-const Page: NextPage<ProductPageProps> = ({ params }) => {
+// Definir el tipo de props para la página
+type Props = {
+  params: PageParams;
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+// Componente de página con tipos correctos
+export default function Page({ params }: Props) {
   const { productID } = params;
   return (
     <div>
       <ProductDetail id={productID} />
     </div>
   );
-};
-
-export default Page;
+}
