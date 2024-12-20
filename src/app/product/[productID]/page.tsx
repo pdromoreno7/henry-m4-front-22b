b9dafb9c98956ab6ae49cd360b27c8be1a1b54d8
pdromoreno7 <pdromorenodev@gmail.com>
2024-12-19 19:09:26 -0500

    fix: async in product page

--- src/app/product/[productID]/page.tsx
+++ src/app/product/[productID]/page.tsx
@@ -1,7 +1,7 @@
 import ProductDetail from "@/app/components/productDetail";
 import React from "react";
 
-async function page({ params }: { params: { productID: string } }) {
+function page({ params }: { params: { productID: string } }) {
   const { productID } = params;
   //   console.log("🚀 ~ page ~ productID:", productID);
   return (
